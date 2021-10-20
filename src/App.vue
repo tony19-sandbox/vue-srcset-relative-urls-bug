2948550625cf9ca472f412f651aac642ffecb036
Tony Trinh <tony19@gmail.com>
2021-10-19 19:04:49 -0500

    demo srcset bug

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img alt="Vue logo" src="@/assets/logo.png" />
+  <img srcset="@/assets/300.png 2x, @/assets/150.png 1x">
 </template>
 
 <style>
